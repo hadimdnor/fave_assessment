9ba3d30a280f2805913e665ef2a135b5e8eab70f
hadimdnor <hadimdnor21@gmail.com>
2021-02-16 22:16:31 +0800

    innitial commit = planning stage

---
+++ planning.rb
@@ -0,0 +1,27 @@
+# requirement 1. 
+    # 1. to come with tax computation 
+    # 2. different salary different tax % 
+    # 3. Then it will return an output in console:
+
+            # Monthly Payslip for: "Ren"
+            # Gross Monthly Income: $5000.00
+            # Monthly Income Tax: $500.00
+            # Net Monthly Income: $4500.00
+
+    # 4. tax bracket 
+            # The Tax Bracket is as follows
+            # Salary Bracket	Rate
+            # first 0 - 20000	0%
+            # next 20001-40000	10%
+            # next 40001-80000	20%
+            # next 80001-180000	30%
+            # 180001 and above	40%
+            # Sample Tax Computation 1
+            # Annual Salary 60000
+    # 5. to create 
+
+# requirement 2.
+
+# requirement 3.
+
+# requirement 4.
